9cb6085b4cd12447135e5fbcb71e70cede2c3811
Josh112406 <quibaljosh@gmail.com>
2026-05-06 01:03:59 +0800

    create class and define functions

---
+++ simple_calculator.py
@@ -0,0 +1,20 @@
+class Calculator:
+    def add(self, num1, num2):
+        pass
+    
+    def subtract(self, num1, num2):
+        pass
+    
+    def multiply(self, num1, num2):
+        pass
+    
+    def divide(self, num1, num2):
+        pass
+    
+    def display_result(self, result):
+        pass
+    
+    def ask_try_again(self):
+        pass
+    
+    
