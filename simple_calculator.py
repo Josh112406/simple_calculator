efcce0acb7a6f40d532224c8f3f0bcda6d30b543
Josh112406 <quibaljosh@gmail.com>
2026-05-06 01:07:01 +0800

    write code for each functions in calculator

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,20 +1,23 @@
 class Calculator:
     def add(self, num1, num2):
-        pass
+        return num1 + num2
     
     def subtract(self, num1, num2):
-        pass
+        return num1 - num2
     
     def multiply(self, num1, num2):
-        pass
+        return num1 * num2
     
     def divide(self, num1, num2):
-        pass
+        return num1 / num2
     
     def display_result(self, result):
-        pass
+        print(f"Result: {result}")
     
     def ask_try_again(self):
-        pass
+        again =  input("Again? (y/n): ").lower()
+        return again == "y"
+    
+    
     
     
